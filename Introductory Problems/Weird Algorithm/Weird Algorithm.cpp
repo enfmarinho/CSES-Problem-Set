d305a5c404706da6194d89619258dab6940be6d5
enfmarinho <eduardonestormarinho228@gmail.com>
2023-11-23 22:49:09 -0300

    Solving Weird Algorithm problem.

---
+++ Introductory Problems/Weird Algorithm/Weird Algorithm.cpp
@@ -0,0 +1,73 @@
+// Problem: Weird Algorithm
+// Contest: CSES Problem Set
+// Judge: CSES
+// URL: https://cses.fi/problemset/task/1068
+// Memory Limit: 512
+// Time Limit: 1000
+// Start: 23/11/2023 22:46
+
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <deque>
+#include <initializer_list>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+#define ll long long
+#define ull unsigned ll
+#define ld long double
+#define F first
+#define S second
+#define FF F.F
+#define FS F.S
+#define SS S.S
+#define SF S.F
+#define range(a) a.begin(), a.end()
+#define GO ios_base::sync_with_stdio(false);
+#define FAST cin.tie(nullptr);
+#define FOR(i, limit) for (int i = 0; i < limit; i++)
+#define MOD 1000000007
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef tuple<ll, ll, ll> tll;
+typedef vector<bool> vb;
+typedef vector<vector<bool>> vvb;
+
+int main() {
+  GO FAST;
+  ll n;
+  cin >> n;
+  cout << n << ' ';
+  while (n != 1) {
+    if (n % 2 == 0) {
+      n /= 2;
+    } else {
+      n = n * 3 + 1;
+    }
+    cout << n << ' ';
+  }
+  return 0;
+}
