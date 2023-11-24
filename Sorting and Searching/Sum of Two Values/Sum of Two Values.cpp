966e23ad809f8e497cfe3643b4659ac93e97a7b8
enfmarinho <eduardonestormarinho228@gmail.com>
2023-11-24 14:55:28 -0300

    Solving the "Sum of Two Values" problem.

---
+++ Sorting and Searching/Sum of Two Values/Sum of Two Values.cpp
@@ -0,0 +1,85 @@
+// Problem: Sum of Two Values
+// Contest: CSES Problem Set
+// Judge: CSES
+// URL: https://cses.fi/problemset/task/1640
+// Memory Limit: 512
+// Time Limit: 1000
+// Start: 24/11/2023 14:44
+
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <deque>
+#include <initializer_list>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+#define ll long long
+#define ull unsigned ll
+#define ld long double
+#define F first
+#define S second
+#define FF F.F
+#define FS F.S
+#define SS S.S
+#define SF S.F
+#define range(a) a.begin(), a.end()
+#define GO ios_base::sync_with_stdio(false);
+#define FAST cin.tie(nullptr);
+#define FOR(i, limit) for (int i = 0; i < limit; i++)
+#define MOD 1000000007
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef tuple<ll, ll, ll> tll;
+typedef vector<bool> vb;
+typedef vector<vector<bool>> vvb;
+
+int main() {
+  GO FAST;
+  int n, x;
+  cin >> n >> x;
+  vpii tabel(n);
+  for (int index{0}; index < n; ++index) {
+    cin >> tabel[index].first;
+    tabel[index].second = index + 1;
+  }
+  sort(range(tabel));
+
+  auto first = tabel.begin();
+  auto last = --tabel.end();
+  while (first != last) {
+    int sum = first->first + last->first;
+    if (sum > x) {
+      --last;
+    } else if (sum < x) {
+      ++first;
+    } else {
+      cout << first->second << ' ' << last->second;
+      return 0;
+    }
+  }
+  cout << "IMPOSSIBLE";
+  return 0;
+}
