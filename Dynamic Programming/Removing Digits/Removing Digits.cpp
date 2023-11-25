701bf3bf68db5c1410b68de16b98132cf94b9d8b
enfmarinho <eduardonestormarinho228@gmail.com>
2023-11-25 01:59:22 -0300

    Solving the "Removing Digits" problem.

---
+++ Dynamic Programming/Removing Digits/Removing Digits.cpp
@@ -0,0 +1,73 @@
+// Problem: Removing Digits
+// Contest: CSES Problem Set
+// Judge: CSES
+// URL: https://cses.fi/problemset/task/1637
+// Memory Limit: 512
+// Time Limit: 1000
+// Start: 25/11/2023 00:50
+
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <deque>
+#include <initializer_list>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+#define ll long long
+#define ull unsigned ll
+#define ld long double
+#define F first
+#define S second
+#define FF F.F
+#define FS F.S
+#define SS S.S
+#define SF S.F
+#define range(a) a.begin(), a.end()
+#define GO ios_base::sync_with_stdio(false);
+#define FAST cin.tie(nullptr);
+#define FOR(i, limit) for (int i = 0; i < limit; i++)
+#define MOD 1000000007
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef tuple<ll, ll, ll> tll;
+typedef vector<bool> vb;
+typedef vector<vector<bool>> vvb;
+
+int main() {
+  GO FAST;
+  int n;
+  cin >> n;
+  vi table(n + 1, INT_MAX);
+  table[0] = 0;
+  for (int i{1}; i <= n; ++i) {
+    string s = to_string(i);
+    for (char c : s) {
+      table[i] = min(table[i], table[i - c + '0'] + 1);
+    }
+  }
+  cout << table[n];
+  return 0;
+}
