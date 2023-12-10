327bf53f7f1a4b2c93af3eae9bec0dcab05f0796
enfmarinho <eduardonestormarinho228@gmail.com>
2023-12-10 13:54:03 -0300

    Solving the "Dynamic Range Sum Queries" problem.

---
+++ Range Queries/Dynamic Range Sum Queries/Dynamic Range Sum Queries.cpp
@@ -0,0 +1,98 @@
+// Problem: Dynamic Range Sum Queries
+// Contest: CSES Problem Set
+// Judge: CSES
+// URL: https://cses.fi/problemset/task/1648
+// Memory Limit: 512
+// Time Limit: 1000
+// Start: 10/12/2023 13:44
+
+#include <iostream>
+#include <tuple>
+#include <utility>
+#include <vector>
+#define ll long long
+#define ull unsigned ll
+#define ld long double
+#define F first
+#define S second
+#define FF F.F
+#define FS F.S
+#define SS S.S
+#define SF S.F
+#define range(a) a.begin(), a.end()
+#define GO ios_base::sync_with_stdio(false);
+#define FAST cin.tie(nullptr);
+#define FOR(i, limit) for (int i = 0; i < limit; i++)
+#define MOD 1000000007
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef tuple<ll, ll, ll> tll;
+typedef vector<bool> vb;
+typedef vector<vector<bool>> vvb;
+
+vll values;
+vll segtree;
+
+ll build(ll index, ll start, ll end) {
+  if (start == end) {
+    return segtree[index] = values[start];
+  }
+  ll mid = (start + end) / 2;
+  return segtree[index] =
+             build(index * 2, start, mid) + build(index * 2 + 1, mid + 1, end);
+}
+
+ll query(ll interval_begin, ll interval_end, ll index, ll start, ll end) {
+  if (interval_end < start or interval_begin > end) {
+    return 0;
+  } else if (interval_begin <= start and interval_end >= end) {
+    return segtree[index];
+  }
+  ll mid = (start + end) / 2;
+  return query(interval_begin, interval_end, 2 * index, start, mid) +
+         query(interval_begin, interval_end, 2 * index + 1, mid + 1, end);
+}
+
+ll update(ll position, ll value, ll index, ll start, ll end) {
+  if (position < start or position > end) {
+    return segtree[index];
+  } else if (start == end) {
+    return segtree[index] = value;
+  }
+  int mid = (start + end) / 2;
+  return segtree[index] = update(position, value, 2 * index, start, mid) +
+                          update(position, value, 2 * index + 1, mid + 1, end);
+}
+
+int main() {
+  GO FAST;
+  ll n, q;
+  cin >> n >> q;
+  values.resize(n);
+  for (ll index = 0; index < n; index++) {
+    cin >> values[index];
+  }
+
+  segtree.resize(4 * n);
+  build(1, 0, n - 1);
+  while (q--) {
+    ll type, first, second;
+    cin >> type >> first >> second;
+    --first;
+    if (type == 1) {
+      update(first, second, 1, 0, n - 1);
+    } else {
+      --second;
+      cout << query(first, second, 1, 0, n - 1) << '\n';
+    }
+  }
+  return 0;
+}
