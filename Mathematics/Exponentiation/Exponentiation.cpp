6c17edec0472262fc4eaf4f1021be10a070f7d3c
enfmarinho <eduardonestormarinho228@gmail.com>
2024-02-16 22:49:12 -0300

    Solving the "Exponentiation" problem.

---
+++ Mathematics/Exponentiation/Exponentiation.cpp
@@ -0,0 +1,57 @@
+// Problem: Exponentiation
+// Contest: CSES Problem Set
+// Judge: CSES
+// URL: https://cses.fi/problemset/task/1095
+// Memory Limit: 512
+// Time Limit: 1000
+// Start: 16/02/2024 22:29
+
+#include <iostream>
+#include <tuple>
+#include <utility>
+#include <vector>
+#define ll long long
+#define ull unsigned ll
+#define ld long double
+#define range(a) a.begin(), a.end()
+#define GO ios_base::sync_with_stdio(false);
+#define FAST cin.tie(nullptr);
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef tuple<ll, ll, ll> tll;
+typedef vector<bool> vb;
+typedef vector<vector<bool>> vvb;
+
+#define MOD 1000000007
+
+ll binpow(ll a, ll b) {
+  if (b == 0) {
+    return 1;
+  }
+  ll r = binpow(a, b / 2);
+  r = r * r % MOD;
+  if (b & 1) {
+    r = r * a % MOD;
+  }
+  return r;
+}
+
+int main() {
+  GO FAST;
+  int n;
+  cin >> n;
+  while (n--) {
+    ll a, b;
+    cin >> a >> b;
+    cout << binpow(a, b) << '\n';
+  }
+  return 0;
+}
